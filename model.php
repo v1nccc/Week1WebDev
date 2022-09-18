388aa2878aebe7c5537e4db6027fe8f653586ed7
v1nccc <vfarellio@student.ciputra.ac.id>
2022-09-18 15:42:51 +0700

    OTW 3

--- model.php
+++ model.php
@@ -5,6 +5,17 @@ public $jabatan;
 public $usia;
 }
 
+class office {
+    public $name;
+    public $adress;
+    public $city;
+    public $phone;
+    }
 
+    class mix {
+        public $employeeid;
+        public $officeid;
+     
+        }
 
 ?>
