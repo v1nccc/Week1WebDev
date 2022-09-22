a73c3106bb7ada51e552b3073611b63a01858df2
v1nccc <vfarellio@student.ciputra.ac.id>
2022-09-22 11:37:09 +0700

    perbaiki header

--- mixedit.php
+++ mixedit.php
@@ -3,16 +3,7 @@ require("controllers.php");
 require("header.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <title>Week 1</title>
-</head>
-<body>
+
 
 <h1 class="text-center mt-2"> Edit Office-Employee </h1>
 <form method="POST" action="mixview.php?edit=<?= $_GET['edit']?>" class="mx-auto  mt-2 w-50">
--- mixview.php
+++ mixview.php
@@ -16,16 +16,7 @@ if(isset($_POST['edit'])){
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <title>Week 1</title>
-</head>
-<body>
+
     <h1 class="text-center"> Office-Employee </h1>
 <table class="table mx-auto table-dark mt-2 w-50">
   <thead>
